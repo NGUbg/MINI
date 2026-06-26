752300fbb9bfcb4637ccf76cdbda6f5bbd336236
NGUbg <giabacngo@gmail.com>
2026-06-26 09:29:28 +0700

    math.py

---
+++ mini-excercises/math.py
@@ -0,0 +1,14 @@
+a = 12
+b = 5
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Remainder:", a % b)
+print("Power:", a ** b)
+
+# Square root using math module
+import math
+print("Square root of a:", math.sqrt(a))
